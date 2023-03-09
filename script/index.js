8365ab9538f5ca4c893d2b39ffc0f6c94939ff4c
Karmanov Egor <karmanov.video@yandex.ru>
2023-03-09 21:26:31 +0700

    fix: поправил замечания ревью, которые я понял

--- script/index.js
+++ script/index.js
@@ -8,7 +8,6 @@ let jobInput = formElement.querySelector("#job");
 const popupCloseButtonElement = formElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__button-edit");
 const popupSaveButtonElement = formElement.querySelector(".popup__btn-save");
-const likeButtonElement = document.querySelector(".element__like-btn");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
@@ -19,13 +18,7 @@ formElement.classList.add('popup__opened');
 const removePopupVisibility = function () {
   formElement.classList.remove('popup__opened');
   }
-popupOpenButtonElement.addEventListener('click', addPopupVisibility);
-popupCloseButtonElement.addEventListener('click', removePopupVisibility);
 
-const toggleLikeStatus = function () {
-  likeButtonElement.classList.toggle('element__like-btn_active');
-}
-likeButtonElement.addEventListener('click', toggleLikeStatus);
 
 
 // Обработчик «отправки» формы, хотя пока
@@ -43,4 +36,5 @@ profileSubtitle.textContent = jobInput.value;
 // // Прикрепляем обработчик к форме:
 // // он будет следить за событием “submit” - «отправка»
  formElement.addEventListener('submit', handleFormSubmit);
-
+ popupOpenButtonElement.addEventListener('click', addPopupVisibility);
+ popupCloseButtonElement.addEventListener('click', removePopupVisibility);
